8f8d58afe94c59862894737933caed4fa489c715
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2017-12-28 00:50:09 +0600

    Deploy website

--- src/data.js
+++
@@ -1,7 +0,0 @@
-export default [
-  { title: "Plane Ticket", value: 268 },
-  { title: "Hotel", value: 199 },
-  { title: "Rental Car", value: 49 },
-  { title: "Dinner", value: 112 },
-  { title: "Postcards", value: 3 }
-];
